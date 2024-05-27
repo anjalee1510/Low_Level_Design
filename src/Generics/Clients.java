6531a890946642bb43737679fc3c4e58c168af7f
Anjalee <anee.anjalee42@gmail.com>
2024-05-27 22:42:26 +0530

    Commit for Generics

---
+++ src/Generics/Clients.java
@@ -0,0 +1,44 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Clients {
+	public static void main(String[] args) {
+		//All the below objects are valid with different data- types because of Generics
+		Pair<Integer , String> p1=new Pair<>();
+		p1.doAgain(90.0,75);
+		Pair<Integer , String> p2=new Pair<>();
+		Pair<String , String> p3=new Pair<>();
+		Pair<Integer , Integer> p4=new Pair<>();
+		
+		p1.setFirst(5);
+		
+		Integer x=p1.getFirst();
+		List<Pair<Integer, String>> list=new ArrayList<>();
+		
+		// Raw types
+		Pair p=new Pair();
+		
+		//Backward compatibility
+		// We shouldn't write the like below
+		HashMap h=new HashMap();
+		h.put("Mohit", 7);
+		h.put(7,"Mohit");
+		// both the above line don't show any error but will create issue
+		
+		//Wrapper classes- Integer,Double, Long - inherit from Object class
+		
+		//Autoboxing
+		int x7=5;
+		Integer x5=x7;
+		
+		String a=Pair.doSomething("Hello");
+		
+		Pair.dodo();
+		
+		
+	}
+
+}
---
+++ src/Generics/Pair.java
@@ -0,0 +1,43 @@
+package Generics;
+
+public class Pair <V,S> {
+	V first;
+	S second;
+	
+	
+//	Pair<V first, S second>{
+//		this.first=first;
+//		this.second=second;
+//	}
+	
+	public void setFirst(V first) {
+		this.first = first;
+	}
+	public void setSecond(S second) {
+		this.second = second;
+	}
+	public V getFirst() {
+		return first;
+	}
+	public S getSecond() {
+		return second;
+	}
+	// Z will not be taken at the time of object creation.
+    // It will be taken at the time of function calling
+	public <Z,Y> Z doAgain(Z temp,Y temp2) {
+		return temp;
+	}
+	
+// V,S will not be available over here hence below won't work
+	// Normal function is written like below
+	//public static void doSomething(T first) {
+	public static <T> T doSomething(T first){
+		return first;
+			
+	}
+	
+	public static <T> void dodo() {
+		T temp=doSomething(null);
+	}
+	
+}
